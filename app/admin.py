975df5d5b47a4d5cd89dd42a2cdb410dd8e68698
euluancesar <luancesarsilva15@gmail.com>
2025-05-29 10:20:21 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -8,43 +8,34 @@ class MatriculaAdmin(admin.ModelAdmin):
     list_filter = ['curso', 'instituicaoEnsino', 'turma']
     search_fields = ['pessoa__nome', 'curso__nome']
 
-
-# i) Ocupação e pessoas
 class PessoaInline(admin.TabularInline):
     model = Pessoa
     extra = 1
 
-# ii) Instituição e cursos
 class CursoInline(admin.TabularInline):
     model = Curso
     extra = 1
 
-# iii) Área do saber e cursos
 class CursoAreaSaberInline(admin.TabularInline):
     model = Curso
     extra = 1
 
-# iv) Cursos e disciplinas
 class CursoDisciplinaInline(admin.TabularInline):
     model = CursoDisciplina
     extra = 1
 
-# v) Disciplinas e avaliações
 class AvaliacaoInline(admin.TabularInline):
     model = Avaliacao
     extra = 1
 
-# vi) Turmas e alunos
 class MatriculaInline(admin.TabularInline):
     model = Matricula
     extra = 1
 
-# vii) UF e cidades
 class CidadeInline(admin.TabularInline):
     model = Cidade
     extra = 1
 
-# ix) Estudantes, disciplinas, avaliações, frequência
 class FrequenciaInline(admin.TabularInline):
     model = Frequencia
     extra = 1
@@ -53,52 +44,45 @@ class OcorrenciaInline(admin.TabularInline):
     model = Ocorrencia
     extra = 1
 
+class InstituicaoEnsinoInline(admin.TabularInline):
+    model = InstituicaoEnsino
+    extra = 1
+
+
 # ========== ADMIN ==========
 
-# i) Ocupação e pessoas
 @admin.register(Ocupacao)
 class OcupacaoAdmin(admin.ModelAdmin):
     inlines = [PessoaInline]
-
-# ii) Instituição e cursos
+    
 @admin.register(InstituicaoEnsino)
 class InstituicaoEnsinoAdmin(admin.ModelAdmin):
-    inlines = [CursoInline]
+    pass
 
-# iii) Área do saber e cursos
 @admin.register(AreaSaber)
 class AreaSaberAdmin(admin.ModelAdmin):
     inlines = [CursoAreaSaberInline]
 
-# iv) Cursos e disciplinas
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
     inlines = [CursoDisciplinaInline]
 
-# v) Disciplinas e avaliações
 @admin.register(Disciplina)
 class DisciplinaAdmin(admin.ModelAdmin):
     inlines = [AvaliacaoInline]
 
-# vi) Turmas e alunos
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
     inlines = [MatriculaInline]
 
-# vii) UF e cidades
-@admin.register(UF)
-class UFAdmin(admin.ModelAdmin):
-    inlines = [CidadeInline]
-
-# ix) Estudantes, disciplinas, avaliações, frequência
 @admin.register(Pessoa)
 class PessoaAdmin(admin.ModelAdmin):
     inlines = [MatriculaInline, FrequenciaInline, OcorrenciaInline]
 
-# Admins simples
 @admin.register(Cidade)
 class CidadeAdmin(admin.ModelAdmin):
     list_display = ['nome']
+    inlines = [InstituicaoEnsinoInline]
 
 @admin.register(AvaliacaoTipo)
 class AvaliacaoTipoAdmin(admin.ModelAdmin):
@@ -107,6 +91,8 @@ class AvaliacaoTipoAdmin(admin.ModelAdmin):
 @admin.register(Matricula)
 class MatriculaAdmin(admin.ModelAdmin):
     list_display = ['pessoa', 'curso', 'instituicaoEnsino', 'turma', 'data_inicio', 'data_previsao_termino']
+    list_filter = ['curso', 'instituicaoEnsino', 'turma']
+    search_fields = ['pessoa__nome', 'curso__nome']
 
 @admin.register(Avaliacao)
 class AvaliacaoAdmin(admin.ModelAdmin):
@@ -130,4 +116,4 @@ class TurnoAdmin(admin.ModelAdmin):
 
 @admin.register(CursoDisciplina)
 class CursoDisciplinaAdmin(admin.ModelAdmin):
-    list_display = ['curso', 'disciplina']
+    list_display = ['disciplina', 'curso']
